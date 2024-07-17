45b6815ea755b677750af9023f07dd3cba1210ca
iam-avinash <110590577+iam-avinash@users.noreply.github.com>
2024-07-17 15:24:26 +0530

    Coffee  Shop Example using Async Await

--- Async Await/index.js
+++ Async Await/index.js
@@ -2,7 +2,7 @@
 
 function placeOrder(drink) {
   return new Promise((resolve, reject) => {
-    if (drink == "coffee") {
+    if (drink === "coffee") {
       resolve("Order for coffee is Placed");
     } else {
       reject("Order not Placed , we dont serve other drinks");
@@ -21,20 +21,35 @@ function generateBill(orderProcessed) {
     resolve(`${orderProcessed} and bill is Rs-500`);
   });
 }
-let placeOrderPromise = placeOrder("coffee");
+// let placeOrderPromise = placeOrder("coffee");
 
-placeOrderPromise
-  .then((orderStatus) => {
-    console.log(orderStatus);
-    return processDrink(orderStatus);
-  })
-  .then((orderProcessed) => {
-    console.log(orderProcessed);
-    return generateBill(orderProcessed);
-  })
-  .then((orderProcessed) => {
-    console.log(orderProcessed);
-  })
-  .catch((error) => {
+// placeOrderPromise
+//   .then((orderStatus) => {
+//     console.log(orderStatus);
+//     return processDrink(orderStatus);
+//   })
+//   .then((orderProcessed) => {
+//     console.log(orderProcessed);
+//     return generateBill(orderProcessed);
+//   })
+//   .then((orderProcessed) => {
+//     console.log(orderProcessed);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//Coffee  Shop Example using Async Await
+
+async function serveOrder() {
+  try {
+    let orderStatus = await placeOrder("coffee");
+    let orderProcessed = await processDrink(orderStatus);
+    let bill = await generateBill(orderProcessed);
+    console.log(bill);
+  } catch (error) {
     console.log(error);
-  });
+  }
+}
+
+serveOrder();
